99c4835e9f9204372473646d99000d757af613e4
banrieen <banrieen@163.com>
2024-11-18 00:25:25 +0800

    生成任务列表数据，可以写入mysql，或csv

--- conf/rpa_db.sql
+++ conf/rpa_db.sql
@@ -4,28 +4,29 @@ CREATE DATABASE IF NOT EXISTS RPA CHARACTER SET utf8mb4 COLLATE utf8mb4_general_
 
 -- Robot 原型构造数据
 -- 指定数据库的字符集，则可以在 `CREATE DATABASE` 语句中使用 `CHARACTER SET` 选项。例如，创建一个使用 UTF-8 编码的数据库：
-CREATE DATABASE IF NOT EXISTS Robot CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+-- Robot 原型构造数据
+-- 指定数据库的字符集，则可以在 `CREATE DATABASE` 语句中使用 `CHARACTER SET` 选项。例如，创建一个使用 UTF-8 编码的数据库：
+-- CREATE DATABASE IF NOT EXISTS Robot CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 -- drop database RPA; 
-SHOW CREATE DATABASE Robot;
+-- SHOW CREATE DATABASE Robot;
 USE Robot;
 drop table task;
 CREATE TABLE if not exists task(     -- 自动化作业任务列表
-    ID      bigint(255),
 	TaskID  varchar(255)  PRIMARY KEY, 
 	TaskName  varchar(255),
 	FlowName  varchar(255) ,
-    AutoBootup bool,              -- 自动拉起开关
+    AutoBootup bool,                 -- 自动拉起开关
     StationModel    varchar(255),    -- 站点型号
     StationID    varchar(255),       -- 站点编号
-	SiteArea  varchar(255),       -- 站点所在区域
-    CurrentLot varchar(255),      -- 站点上当前批次的编号
-    CurrentStep varchar(255),     -- 站点当前作业工步（工序）
+	SiteArea  varchar(255),          -- 站点所在区域
+    CurrentLot varchar(255),         -- 站点上当前批次的编号
+    CurrentStep varchar(255),        -- 站点当前作业工步（工序）
     RunMethod varchar(255),
-    `TaskStatus`  varchar(255),   -- 作业状态： 运行，完成，异常；
+    `TaskStatus`  varchar(255),      -- 作业状态： 运行，完成，异常；
     Priority  INT,
-    ExtendedVar  json,            -- 自定义参数, 扩展参数
-    `Schedule` varchar(255),      -- 例行执行计划 
-    AlarmRule  bool,              -- 是否发送告警
+    ExtendedVar  json,               -- 自定义参数, 扩展参数
+    `Schedule` varchar(255),         -- 例行执行计划 
+    AlarmRule  bool,                 -- 是否发送告警
 	StartTime  DATETIME,
 	Endtime  DATETIME,
 	OperatorID  varchar(255),
@@ -33,63 +34,67 @@ CREATE TABLE if not exists task(     -- 自动化作业任务列表
 );
 drop table task_log;
 CREATE TABLE if not exists task_log(   -- 自动化作业日志记录
-    ID      bigint(255),
 	TaskID  varchar(255)  PRIMARY KEY, 
 	TaskName  varchar(255),
     `TaskStatus`  varchar(255),
 	FlowName  varchar(255) ,
     StationID    varchar(255),       -- 站点编号
-    CurrentLot varchar(255),      -- 站点上当前批次的编号
-    CurrentStep varchar(255),     -- 站点当前作业工步（工序）
-    WaferID    varchar(255),      -- 当前作业的晶圆片号
-    ExeStep    varchar(255),      -- 执行步骤
-    ExeTimes   INT,               -- 重试次数
-    ExeRsp     json,              -- 执行结果
-	ExeTime  DATETIME,            -- 执行时间
-    Image    varchar(1024)        -- 快照图像链接
+    CurrentLot varchar(255),         -- 站点上当前批次的编号
+    CurrentStep varchar(255),        -- 站点当前作业工步（工序）
+    WaferID    varchar(255),         -- 当前作业的晶圆片号
+    ExeStep    varchar(255),         -- 执行步骤
+    ExeRetryTimes   INT,             -- 重试次数
+    ExeRsp     json,                 -- 执行结果
+	ExeTime  DATETIME,               -- 执行时间
+    Image    varchar(1024)           -- 快照图像链接
 );
-
-
-CREATE TABLE if not exists flow(     -- 流程列表
+drop table flow;
+CREATE TABLE if not exists flow(         -- 流程列表
 	FlowID  varchar(255)  PRIMARY KEY,   -- 流程编号
-	FlowName  varchar(255) ,      -- 流程名称
-    FlowPath  varchar(1024) ,     -- 流程配置或脚本文件路径
-    UpdateTime  DATETIME,         -- 更新时间
-    ScriptType   varchar(255),    -- 脚本文件类型 .py, .tar
-    Scriptpath   varchar(1024),   -- 脚本上传路径
-    UseCase    varchar(255),      -- 适用场景
-    FlowStatus    varchar(255),   -- 流程开发状态：开发中，验证中，已发布，废弃
-	Version  varchar(255),        -- 版本号
-    Maintainer varchar(255),      -- 维护人
-    StartRule  varchar(255),      -- 触发规则
-    RetryInternal INT,            -- 重试时间间隔
-    CheckFirstSlot bool,              -- 首片检测开关
-    AlarmIinfo   varchar(255),     -- 特殊告警信息
-    ExampeImage varchar(255),      -- 示例快照图像
-    ExtendedVar  json,            -- 自定义参数, 扩展参数
-    Note text                     -- 流程描述，备注信息
+	FlowName  varchar(255) ,             -- 流程名称
+    FlowPath  varchar(1024) ,            -- 流程配置或脚本文件路径
+    UpdateTime  DATETIME,                -- 更新时间
+    ScriptType   varchar(255),           -- 脚本文件类型 .py, .tar
+    Scriptpath   varchar(1024),          -- 脚本上传路径
+    UseCase    varchar(255),             -- 适用场景
+    FlowStatus    varchar(255),          -- 流程开发状态：开发中，验证中，已发布，废弃
+	Version  varchar(255),               -- 版本号
+    Maintainer varchar(255),             -- 维护人
+    StartRule  varchar(255),             -- 触发规则
+    RetryInternal INT,                   -- 重试时间间隔
+    CheckFirstSlot bool,                 -- 首片检测开关
+    AlarmIinfo   varchar(255),           -- 特殊告警信息
+    ExampeImage varchar(255),            -- 示例快照图像
+    ExtendedVar  json,                   -- 自定义参数, 扩展参数
+    Note text                            -- 流程描述，备注信息
 );
-
-CREATE TABLE if not exists station(     -- 设备站点列表
-    ID      bigint(255),
+drop table station;
+CREATE TABLE if not exists station(       -- 设备站点列表
 	StationID  varchar(255)  PRIMARY KEY,  -- 站点编号 
 	StationModel  varchar(255),            -- 站点型号
     StationMAC  varchar(255) ,             -- 站点MAC
-	StationIP  varchar(255) ,      -- 站点IP
-    StationAccount varchar(255) ,  -- 站点访问账号
-    StationStatus varchar(255) ,  -- 站点状态
-    TestID        varchar(255) ,  -- 测试机编号
-    TestModel     varchar(255) ,  -- 测试机型号
-    TestMac       varchar(255) ,  -- 测试机MAC
-    TestIP        varchar(255) ,  -- 测试机IP
-    TestAccount   varchar(255) ,  -- 测试机账号
-    TestPinCardID varchar(255) ,  -- 针卡编号
-    StationColorLamp  varchar(255) ,  -- 三色灯状态
-    CurrentLot varchar(255),      -- 站点上当前批次的编号
-    CurrentStep varchar(255),     -- 站点当前作业工步（工序）
-    `TaskStatus`  varchar(255),   -- 作业状态： 运行，完成，异常；
-    ExtendedVar  json,            -- 自定义参数, 扩展参数
-	UpTime  DATETIME,             -- 站点上线时间
-	Maintainer  varchar(255),      -- 站点负责人
+	StationIP  varchar(255) ,              -- 站点IP
+    StationAccount varchar(255) ,          -- 站点访问账号
+    StationStatus varchar(255) ,           -- 站点状态
+    TestID        varchar(255) ,           -- 测试机编号
+    TestModel     varchar(255) ,           -- 测试机型号
+    TestMac       varchar(255) ,           -- 测试机MAC
+    TestIP        varchar(255) ,           -- 测试机IP
+    TestAccount   varchar(255) ,           -- 测试机账号
+    TestPinCardID varchar(255) ,           -- 针卡编号
+    StationColorLamp  varchar(255) ,       -- 三色灯状态
+    CurrentLot varchar(255),               -- 站点上当前批次的编号
+    CurrentStep varchar(255),              -- 站点当前作业工步（工序）
+    `TaskStatus`  varchar(255),            -- 作业状态： 运行，完成，异常；
+    ExtendedVar  json,                     -- 自定义参数, 扩展参数
+	UpTime  DATETIME,                      -- 站点上线时间
+	Maintainer  varchar(255),              -- 站点负责人
     Note text
 );
+
+
+-- Debug 
+SELECT * FROM Robot.task;
+
+INSERT INTO Robot.task (TaskID, TaskName, FlowName, AutoBootup, StationModel, StationID, SiteArea, CurrentLot, CurrentStep, RunMethod, TaskStatus, Priority, ExtendedVar, `Schedule`, AlarmRule, StartTime, Endtime, OperatorID, Note) 
+VALUES (60422626, '作业-154', '流程-8C5', '否', '运行中', 'WX-172', '二楼', '3429-9923-', 'CP6', '上线', '运行中', 100, '[]', '25 23 25 9 5', '调试流程：内部推送记录', '2024-11-17 23:41:03', '2024-11-17 23:59:21', '26C3D5', '验证作业：已经调试完成的自动化作业流程需要安排2~10台站点，小批量上线验证，务必有操作员和验证人员在线监视，观察执行中的异常情况和收集数据。');
--- generater/rand_task.py
+++ generater/rand_task.py
@@ -4,13 +4,117 @@
 数据源管理
 数据加载
 数据生成
-
+导出到 csv
 """
 
-# 
-import polars as pl
-uri = "mysql://thomas:thomas@192.168.56.113:3306/rpa_vt"
-query = "SELECT * FROM rpa_vt.flow_target_record;"
+# from src.mysqlSync import mysql_sync
+# from src.csvParser import csvParser
+# import toml
+import random
+import pdb
 
-pl.read_database_uri(query=query, uri=uri)
-print(pl)
+## 查询数据
+# import polars as pl
+# uri : "mysql://thomas:thomas@192.168.56.113:3306/rpa_vt"
+# query : "SELECT * FROM rpa_vt.flow_target_record;"
+
+# pl.read_database_uri(query:query, uri:uri)
+# print(pl)
+import concurrent.futures
+import polars as pl
+import datetime
+import json
+from mysqlSync import mysql_sync
+
+def get_conf(conf_path="conf/rpa_datatable.toml"):
+    import toml
+    # 读取配置文件
+    with open(conf_path, 'r') as f:
+        config = toml.load(f)
+        return config
+
+def gen_batch_row(func_batch, batch=1000):
+    # 批量生成数据 df
+    with concurrent.futures.ThreadPoolExecutor(max_workers=(batch/10)) as executor:
+        futures = [executor.submit(func_batch) for _ in range(batch)]
+    results = [f.result() for f in futures]
+    df = pl.DataFrame(results)
+    return df
+
+def export_csv(gen_data_df, csf_path="output.csv"):
+    # 导出数据 df 到本地 csv 表格
+    gen_data_df.write_csv("output.csv",include_header=True,separator=",")
+
+def write_mysql(gen_data_df,table_name,conf,):
+    # Init mysql connection
+    my_client = mysql_sync() 
+    my_client.conn(conf["mariadb"])
+    data_schema = ', '.join(conf[table_name].keys())
+    insert_sql = "INSERT INTO %s (%s) VALUES (%s);" % (table_name, data_schema, ', '.join(['%s'] * len(gen_data_df.columns)))
+    data_tuples = [tuple(row) for row in gen_data_df.rows()]
+    # breakpoint()
+    my_client.execute_many(insert_sql, gen_data_df.rows())
+    my_client.cnx.commit()
+    pass
+
+def random_cron():
+    # 构造随机 cron 计划表达式
+    minute = random.randint(0, 59)
+    hour = random.randint(0, 23)
+    day_of_month = random.randint(1, 31)
+    month = random.randint(1, 12)
+    day_of_week = random.randint(0, 6)
+    return f"{minute} {hour} {day_of_month} {month} {day_of_week}"
+
+def random_datetime(start_time=None,time_scope=30):
+    # 构造一定时长内的随机时间
+    now = start_time if start_time else datetime.datetime.now()
+    start_time = now - datetime.timedelta(minutes=time_scope)
+    t = start_time + datetime.timedelta(
+        seconds=random.randint(0, 59),
+        minutes=random.randint(0, 29))
+    if (t > now) and (start_time==None):
+        return "None"                                        ## 如果是生成启动时间，不设置start_time, 生成时间不宜晚于当前
+    elif(t < now) and (start_time==None):
+        return "None"                                        ## 如果是生成完成时间，设置start_time,生成时间不宜早于当前
+    else:
+        return (t.strftime("%Y-%m-%d %H:%M:%S"))
+
+def gen_task_list():
+    # 构造数据
+    task_list = {
+                "TaskID" : random.randint(10000000, 99999999),
+                "TaskName" : "作业-"+str(random.randint(0, 200)),
+                "FlowName" : "流程-"+''.join(random.sample('0123456789ABCDEF', 3)),
+                "AutoBootup" : random.choice(["是","否"]),
+                "StationModel" : random.choice(["运行中","空闲","停机","故障",]),
+                "StationID" : "WX-"+str(random.randint(0, 400)),
+                "SiteArea" :  random.choice(["二楼","三楼"]),
+                "CurrentLot" : ''.join(random.sample('0123456789ABCDEF', 4)) + "-" + str(random.randint(1000, 9999)) + "-",
+                "CurrentStep" : random.choice(["CP1","CP2","CP3","CP4","CP5","CP6",]),
+                "RunMethod" : random.choice(["调试","验证中","上线"]),
+                "TaskStatus" :  random.choice(["运行中","已完成","已下线","故障",]),
+                "Priority" : 100,
+                "ExtendedVar" : json.dumps([]),    ## Json dump as string
+                "Schedule" : random_cron(),
+                "AlarmRule" : random.choice(["验证流程：推送小组工作群","已发布上线：推送生产工作群","调试流程：内部推送记录",]),
+                "StartTime" : random_datetime(time_scope=45),
+                "Endtime" : random_datetime(datetime.datetime.now(),time_scope=15),
+                "OperatorID" : ''.join(random.sample('0123456789ABCDEF', 6)),
+                "Note" : random.choice(["调试作业：请与产线主管，QA协调借机，工程片，配套测试仪和针卡，在规定的时间内完成调试排查。",
+                          "验证作业：已经调试完成的自动化作业流程需要安排2~10台站点，小批量上线验证，务必有操作员和验证人员在线监视，观察执行中的异常情况和收集数据。",
+                          "上线作业: 已经验证通过的自动化作业流程，必须通知生产，质量和工艺主管确认后，与生产负责人协调上线，前期要求留人观察。" ]),
+                             }
+    return task_list
+    
+
+if __name__ == "__main__":
+    # gen_task_list()
+    csf_path = ""
+    batch = 10000
+    table_name = "task"   # mariadb
+    conf = get_conf(conf_path="conf/rpa_datatable.toml")
+    # print(conf[table_name])
+    gen_data_df = gen_batch_row(gen_task_list, batch)
+    # export_csv(gen_data_df)
+    write_mysql(gen_data_df,table_name,conf,)
