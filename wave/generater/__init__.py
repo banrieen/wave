6434c83d46a3e31f1d020325efa49c90163e1e39
thomas <banrieen@163.com>
2024-12-24 23:53:40 +0800

    change license

---
+++ wave/generater/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# src Python Library for personaly ETL, (C)
+# 2025 Thomas.
+#
+#                    GNU GENERAL PUBLIC LICENSE
+#                       Version 3, 29 June 2007
+#
+# Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+# Everyone is permitted to copy and distribute verbatim copies
+# of this license document, but changing it is not allowed.
+
+# pylint: disable=too-many-lines,disable=too-many-branches,too-many-statements
+# pylint: disable=too-many-arguments
+
+import rand_task
